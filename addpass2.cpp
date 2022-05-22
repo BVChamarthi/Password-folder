05168de63a199063a33c91a07f4b9eedf14de2cd
BVChamarthi <105806736+BVChamarthi@users.noreply.github.com>
2022-05-22 14:36:39 -0400

    Update addpass2.cpp
    added password creation when program is first called

--- addpass2.cpp
+++ addpass2.cpp
@@ -13,6 +13,22 @@ int main() {
         fs::create_directory(folder);
         system("attrib +s +h \"locker\"");          // add the hidden tag and the system tag to the file
     }
+    
+    // CHECK IF THE USER CREATED A PASSWORD
+    fs::path password_path("./locker/password.dat");
+    if(!fs::exists(password_path)) {                        // if the user hasn't created a password :
+        std::fstream password_file;
+        password_file.open("./locker/password.dat", std::ios::out | std::ios::binary);
+        std::cout << "Create new Password : ";              // ask the user to create a new password and store it in the protected folder itself
+        std::string password;
+        getline(std::cin, password);
+        password_file << password;
+        password_file.close();
+        
+        system("attrib +s +h +r \"locker/password.dat\"");          // add the hidden, system and read-only tags to the password file
+        
+        std::cout << "New password set\n";
+    }
 
     bool hidden = GetFileAttributesA((LPCSTR)"locker") & FILE_ATTRIBUTE_HIDDEN;     // check if the file is hidden
     
@@ -20,8 +36,14 @@ int main() {
         std::string passTry;
         std::cout << "Enter password : ";
         getline(std::cin, passTry);                 // take in a password in the shell
+        
+        std::fstream password_file;                               // get the actual password from the password file
+        password_file.open("./locker/password.dat", std::ios::in | std::ios::binary);
+        std::string password;
+        getline(password_file, password);
+        password_file.close();
 
-        if(passTry.compare("joinme") == 0){         // check if the password is the required password (here 'joinme')
+        if(passTry.compare(password) == 0){         // check if the password is the required password (here 'joinme')
             system("attrib -s -h \"locker\"");      // remove the file's hidden (-h) and system (-s) tags.
                                                     // the hidden tag hides the file and the system tag will make it read only
                                                     // which makes it so that one cannot unhide the file with the file explorer options
