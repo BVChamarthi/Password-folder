3ea5aaa6145a36189af67651763f189597656e68
BVChamarthi <105806736+BVChamarthi@users.noreply.github.com>
2022-05-22 14:00:49 -0400

    Update addpass2.cpp

--- addpass2.cpp
+++ addpass2.cpp
@@ -18,17 +18,22 @@ int main() {
     
     if(hidden) {                                    // if the file is hidden
         std::string passTry;
+        std::cout << "Enter password : ";
         getline(std::cin, passTry);                 // take in a password in the shell
 
         if(passTry.compare("joinme") == 0){         // check if the password is the required password (here 'joinme')
             system("attrib -s -h \"locker\"");      // remove the file's hidden (-h) and system (-s) tags.
                                                     // the hidden tag hides the file and the system tag will make it read only
                                                     // which makes it so that one cannot unhide the file with the file explorer options
+            std::cout << "Folder is unlocked\n";
             return 0;           // exit the program
+        } else {
+            std::cout << "Incorrect password\n";
+            return 0;
         }
     }
 
     system("attrib +s +h \"locker\"");          // add the hidden tag and the system tag to the file
-
+    std::cout << "Folder is locked\n";
     return 0;
 }
